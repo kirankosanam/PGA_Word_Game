1f7784ce63f94f1df9fd092d928c4edfb1c07595
kirankosanam <32774508+kirankosanam@users.noreply.github.com>
2020-12-03 10:38:33 +0530

    Update kiran.py

--- PycharmProjects/PGA_Word_Game/kiran.py
+++ PycharmProjects/PGA_Word_Game/kiran.py
@@ -1,9 +1,11 @@
 import random
 
+spaces = 3
+
 
 def generate_random_locations(n, n_stars):
     lis = list()
-    while len(lis) < n_stars:
+    while len(lis) < n_stars + 1:
         x = random.randint(0, n - 1)
         if x not in lis:
             lis.append(x)
@@ -14,16 +16,101 @@ def generate_miss_word(word):
     max_length = len(word)
     max_spaces = max_length // 2
     locations = generate_random_locations(max_length, max_spaces)
-    print(locations, "locations")
+    # print(locations, "locations")
     word_list = list(word)
     for i in locations:
         word_list[i] = '*'
     missed_word = ''.join(word_list)
-    print(missed_word)
+    # print(missed_word)
+    return [missed_word, len(locations)]
 
 
-name = input("What is your name? ")
-print("Good Luck ! ", name)
+def select_difficulty(simplicity):
+    rw = ''
+    if simplicity == 1:
+        rw = random.choice(words)
+        while True:
+            rw = random.choice(words)
+            if len(rw) <= 7:
+                break
+        return rw
+
+    elif simplicity == 2:
+        rw = random.choice(words)
+        while True:
+            rw = random.choice(words)
+            if 5 < len(rw) <= 9:
+                break
+        return rw
+
+    elif simplicity == 3:
+        rw = random.choice(words)
+        while True:
+            rw = random.choice(words)
+            if 9 < len(rw):
+                break
+        return rw
+    else:
+        raise IndexError
+
+
+def select_trails(simplicity, loc):
+    if simplicity == 1:
+        return loc * 2
+
+    elif simplicity == 2:
+        return loc + (loc // 2)
+
+    elif simplicity == 3:
+        return loc
+
+    else:
+        raise IndexError
+
+
+def play(original_word, modified_word, no_of_trails):
+    original_word = original_word
+    modified_word = modified_word
+    no_of_trails = no_of_trails
+    missing_numbers = list()
+    missing_numbersindex = list()
+
+    for i in range(0, len(original_word)):
+        if (original_word[i] != modified_word[i]):
+            missing_numbers.append(original_word[i])
+            missing_numbersindex.append(i)
+
+    for j in range(0, no_of_trails):
+        if len(missing_numbers) != 0:
+            enter_character = input(f"Enter Character {modified_word}:")
+            if enter_character in missing_numbers:
+                k = missing_numbers.index(enter_character)
+                m = int(missing_numbersindex[k])
+                missing_numbers.remove(enter_character)
+                modifiedtest = list(modified_word)
+                modifiedtest[m] = enter_character
+                modified_word1 = "".join(modifiedtest)
+                modified_word = modified_word1
+                print("Correct And Word is: " + modified_word)
+                print(f'The Remaining Trails Are {no_of_trails - j - 1}.')
+                missing_numbersindex.remove(m)
+
+            else:
+                if j < no_of_trails - 1:
+                    print("Wrong Try Again")
+                    print(f'The Remaining Trails Are {no_of_trails - j - 1}.')
+                else:
+                    print("Nah! Try again\nYou Loose")
+        else:
+            if len(missing_numbers) == 0:
+                print("You WIN And The Word Is: " + modified_word)
+                break
+    print("The Original Word is:" + original_word)
+
+
+random_word = []
+n = input("What is your name? ")
+print("Good Luck ! ", n)
 words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player', 'condition',
          'reverse', 'water', 'board', 'geeks', 'alcohol', 'copy', 'paste', 'milk', 'empty', 'floor', 'come', 'hundred',
          'coding', 'getup', 'vegetables', 'spacebar', 'control', 'oldman', 'talking', 'eating', 'wnated', 'lie',
@@ -65,29 +152,19 @@ words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics
          'advertisement', 'cyber', 'wheel', 'bullockcart', 'building', 'education', 'environment', 'notes',
          'individual', 'new', 'refresh', 'website', 'lightup', 'laugh', 'lighthouse', 'beach', 'continent', 'pacific',
          'noun', 'verb', 'nature', 'herbal', 'lifetime', 'understanding', 'sucess', 'successful']
-simplicity = int(input("Enter\n1 -> Simple \n2 -> Medium \n3 -> Hard \n"))
-random_word = ''
-if simplicity == 1:
-    random_word = random.choice(words)
-    while True:
-        random_word = random.choice(words)
-        if len(random_word) <= 5:
-            break
-elif simplicity == 2:
-    random_word = random.choice(words)
-    while True:
-        random_word = random.choice(words)
-        if 5 < len(random_word) <= 7:
-            break
-elif simplicity == 3:
-    random_word = random.choice(words)
-    while True:
-        random_word = random.choice(words)
-        if 7 < len(random_word):
-            break
+diff = int(input("Enter\n1 -> Simple \n2 -> Medium \n3 -> Hard \n"))
+try:
+    random_word.append(select_difficulty(diff))
+except IndexError:
+    print("Invalid selection range")
+random_word.append(generate_miss_word(random_word[0]))
 
-else:
-    print("Please enter the difficulty correctly")
+# print(random_word)
 
-print(random_word)
-generate_miss_word(random_word)
+r_word = random_word[0]
+r_trails = select_trails(diff, random_word[1][1])
+rm_word = random_word[1][0]
+# print(r_word)
+print(f'You only have {r_trails} trails')
+# print(random_word[1][0])
+play(r_word, rm_word, r_trails)
